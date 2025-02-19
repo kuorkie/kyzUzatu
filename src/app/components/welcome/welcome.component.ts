72b2533406f872771e72ac905df8f7ffcf15dea2
yeldarl2808 <6cdh5j-TR9znY0>
2025-02-19 18:12:55 +0500

    gh

--- src/app/components/welcome/welcome.component.ts
+++ src/app/components/welcome/welcome.component.ts
@@ -9,5 +9,11 @@ import {RsvpComponent} from '../rsvp/rsvp.component';
   styleUrl: './welcome.component.css'
 })
 export class WelcomeComponent {
+  isMobile:boolean = window.innerWidth < 768
+  constructor() {
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth < 768;
+    });
 
+  }
 }
